3d7144308fc03905f0ed4cdb1f57f8a2146dab6f
Marcos Eugênio <marcos.eugenioln@gmail.com>
2023-03-28 20:08:36 -0300

    add db file

--- database.py
+++ database.py
@@ -4,6 +4,10 @@ class Database():
 
     def __init__(self):
         print('starting database...')
+        connection = sqlite3.connect('database.db')
+        self.query = connection.cursor()
+        
 
     def create_tables(self):
         print('creating tables...')
+        self.query.execute('CREATE TABLE table')
