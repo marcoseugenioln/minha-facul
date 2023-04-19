c8429957b9b25a20a7da4d38b938d4e43df9b580
Marcos Eugênio <marcos.eugenioln@gmail.com>
2023-04-19 17:56:21 -0300

    register screen

--- index.py
+++ index.py
@@ -11,6 +11,10 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
  
 if __name__ == '__main__':
     app.run(debug=True)
