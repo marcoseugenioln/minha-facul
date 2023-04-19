6e916f9cb69dc1971de9596eee9a300c4aea94d1
Marcos Eugênio <marcos.eugenioln@gmail.com>
2023-04-19 17:24:04 -0300

    login page

--- index.py
+++ index.py
@@ -7,6 +7,10 @@ site = Blueprint('site', __name__, template_folder='templates')
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
  
 if __name__ == '__main__':
     app.run(debug=True)
